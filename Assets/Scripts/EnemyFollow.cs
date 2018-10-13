0c6c76b40962200fa82137a74542ae55826c0d2d
Jaroslav Urban <jaroslav.urbann@gmail.com>
2018-10-13 17:43:12 +0200

    Tower UI done

--- Assets/Scripts/EnemyFollow.cs
+++ Assets/Scripts/EnemyFollow.cs
@@ -4,35 +4,28 @@ using UnityEngine;
 
 public class EnemyFollow : MonoBehaviour {
 
-	public float speed;
-	public float personalSpace;
-    public Transform[] waypoints;
+	public float Speed;
+    public Transform[] Waypoints;
 	private int waypointIndex = 0;
-
-	private Transform target;
 
 	// Use this for initialization
 	void Start () {
-		target = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
-		transform.position = waypoints[waypointIndex].transform.position;
+		transform.position = Waypoints[waypointIndex].transform.position;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		FollowWaypoints();
-		if(Vector2.Distance(transform.position, target.position) > personalSpace && waypointIndex > waypoints.Length - 1){
-			transform.position = Vector2.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
-		}
 	}
 
 	private void FollowWaypoints()
     {
-        if (waypointIndex <= waypoints.Length - 1)
+        if (waypointIndex <= Waypoints.Length - 1)
         {
             transform.position = Vector2.MoveTowards(transform.position,
-               waypoints[waypointIndex].transform.position,
-               speed * Time.deltaTime);
-            if (transform.position == waypoints[waypointIndex].transform.position)
+               Waypoints[waypointIndex].transform.position,
+               Speed * Time.deltaTime);
+            if (transform.position == Waypoints[waypointIndex].transform.position)
             {
                 waypointIndex += 1;
             }
