45ca77aeedf6974e3fd2d475e0c847ee6462c186
Jaroslav Urban <jaroslav.urbann@gmail.com>
2018-10-28 22:28:14 +0100

    Added menus structure

---
+++ Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour {
+
+	bool isPaused = false;
+	public GameObject PauseUI;
+
+	void Start(){
+		Resume();
+	}
+
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.Escape)){
+			if (isPaused){
+				Resume();
+			}
+			else{
+				Pause();
+			}
+		}
+	}
+
+	public void LevelsMenu(){
+		SceneManager.LoadScene("LevelsMenu");
+	}
+
+	public void Resume(){
+		PauseUI.SetActive(false);
+		Time.timeScale = 1f;
+		isPaused = false;
+	}
+
+	void Pause(){
+		PauseUI.SetActive(true);
+		Time.timeScale = 0f;
+		isPaused = true;
+	}
+}
