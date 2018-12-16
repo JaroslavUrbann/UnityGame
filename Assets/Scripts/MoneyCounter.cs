b36d446ca2f29804b159ddf606b6faabd2cfd69b
Jaroslav Urban <jaroslav.urbann@gmail.com>
2018-12-16 12:56:19 +0100

    enemy animations added

---
+++ Assets/Scripts/MoneyCounter.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public static class Money {
+
+	public static float Amount;
+
+	public static bool Buy(float price){
+		if(Money.Amount - price >= 0){
+			Money.Amount -= price;
+			return true;
+		}
+		return false;
+	}
+}
+
+public class MoneyCounter : MonoBehaviour {
+
+	public float StartMoney;
+	public Text MoneyText;
+
+	void Start () {
+		Money.Amount = StartMoney;
+	}
+	
+	void Update () {
+		MoneyText.text = Money.Amount.ToString() + "$";
+	}
+}
